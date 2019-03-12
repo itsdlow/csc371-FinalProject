75888902168cbdbc369069419809a707eda16ee6
DeAngelo Wilson <deangelo76er@sbcglobal.net>
2019-03-12 17:51:12 -0500

    basic tableview cell set + basiccalccontroller

---
+++ csc371-FinalProject-AADW/csc371-FinalProject-AADW/BasicCalculatorViewController.swift
@@ -0,0 +1,109 @@
+//
+//  BasicCalculatorViewController.swift
+//  csc371-FinalProject-AADW
+//
+//  Created by DeAngelo Deshawn Wilson on 3/6/19.
+//  Copyright © 2019 DeAngelo Deshawn Wilson. All rights reserved.
+//
+
+import UIKit
+
+class BasicCalculatorViewController: UIViewController {
+    
+    
+    enum operations : String{
+        case addition = "+"
+        case subtraction = "-"
+        case multiplication = "*"
+        case division = "/"
+    }
+
+    var o:operations?
+    var left:Double?
+    var right:Double?
+    var result:Double=0
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var clearBtn: UIButton!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var subBtn: UIButton!
+    @IBOutlet weak var equalsBtn: UIButton!
+    
+    @IBOutlet var numBtns: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clear(_ sender: UIButton) {
+    }
+
+    //operatioj function, must take care of previous value, stored in resultLabel
+    @IBAction func op(_ sender: UIButton) {
+        //store resultLabel num into equation
+        guard let label = sender.titleLabel?.text else {return}
+        switch label {
+        case "+":
+            o = .addition
+        case "-":
+            o = .subtraction
+        case "*":
+            o = .multiplication
+        case "/":
+            o = .division
+        default:
+            break
+        }
+        
+        
+    }
+
+    
+    @IBAction func number(_ sender: UIButton) {
+        if let numStr = sender.titleLabel?.text{
+            resultLabel.text?.append(numStr)
+            //equation.append(num)
+        }
+    }
+    
+    @IBAction func equals(_ sender: UIButton) {
+        guard let l = left else {return}
+        if let r = Double(resultLabel.text!){
+            right=r
+            if let operation = o{
+                switch operation{
+                case .addition:
+                    result = add(pre: l, cur: r)
+                case .subtraction:
+                    result = sub(pre: l, cur: r)
+                default:
+                    break
+                }
+            }
+
+        }
+    }
+    
+
+    
+    
+    
+    func add (pre:Double, cur:Double) -> Double {
+        return pre+cur
+    }
+    func sub (pre:Double, cur:Double) -> Double {
+        return pre-cur
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
